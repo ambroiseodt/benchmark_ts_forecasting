e789c125431d3cbfda8b88cb486ef8973010aab0
jadyehya <jadyehya@hotmail.com>
2024-06-03 15:12:08 +0200

    Added data_windowing routine

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -3,5 +3,38 @@
 # name `benchmark_utils`, and code defined inside will be importable using
 # the usual import syntax
 
-def gradient_ols(X, y, beta):
-    return X.T @ (X @ beta - y)
+import numpy as np
+
+
+def data_windowing(
+    data: np.ndarray,
+    window_size: int,
+    step: int,
+    horizon: int,
+):
+    """
+    Apply windowing to the data
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Data to be windowed
+    window_size : int
+        Size of the window
+    step : int
+        Step size between windows
+    horizon : int
+        Number of steps to forecast
+    """
+    X = np.lib.stride_tricks.sliding_window_view(
+        data, window_shape=window_size, axis=0
+    )[::step]
+
+    labels = np.lib.stride_tricks.sliding_window_view(
+        data[window_size:], window_shape=window_size, axis=0
+    )[::step][:, :, :horizon]
+
+    # Remove the last windows that do not have labels
+    data = X[: -(len(X) - len(labels))]
+
+    return data, labels
