a8904bf95ff6410d69bf18e1f5e39a2e3a784180
Cédric Allain <CedricAllainEnsae@users.noreply.github.com>
2024-06-03 14:24:09 +0200

    modified simulated dataset + add first attempt for bike_sharing_demand dataset

--- datasets/bike_sharing_demand.py
+++ datasets/bike_sharing_demand.py
@@ -1,5 +1,9 @@
 from benchopt import BaseDataset, safe_import_context
 
+from sklearn.datasets import fetch_openml
+
+with safe_import_context() as import_ctx:
+    from benchmark_utils import data_windowing
 
 # Protect the import with `safe_import_context()`. This allows:
 # - skipping import to speed up autocompletion in CLI.
@@ -12,17 +16,15 @@ with safe_import_context() as import_ctx:
 class Dataset(BaseDataset):
 
     # Name to select the dataset in the CLI and to display the results.
-    name = "Simulated"
+    name = "Bike_Sharing_Demand"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
     # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
-        'n_features': [5],
-        'n_windows': [10],
+        'train_size': [0.7],
         'window_size': [100],
-        'forecast_size': [10],
-        'random_state': [42],
+        'stride': [1]
     }
 
     # List of packages needed to run the dataset. See the corresponding
@@ -34,12 +36,16 @@ class Dataset(BaseDataset):
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
 
-        # Generate pseudorandom data using `numpy`.
-        rng = np.random.RandomState(self.random_state)
+        # Fetch dataset from OpenML
+        # https://www.openml.org/search?type=data&status=active&id=44063
+        bike_sharing = fetch_openml("Bike_Sharing_Demand", version=2, as_frame=True)
+        df = bike_sharing.frame
 
-        # Split the data
-        X = rng.randn(self.n_features, self.window_size, self.n_windows)
-        y = rng.randn(self.n_features, self.forecast_size, self.n_windows)
+        # Only keep a subsample of variables, the quantitative ones
+        columns = ['temp', 'feel_temp', 'humidity', 'windspeed', 'count']
+        df = df[columns]
+
+        X, y = data_windowing(df)
 
         # The dictionary defines the keyword arguments for `Objective.set_data`
         return dict(X=X, y=y)
