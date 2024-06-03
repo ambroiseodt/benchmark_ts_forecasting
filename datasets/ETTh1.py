527a277a42e802c73701726567aa1030229f8d73
jadyehya <jadyehya@hotmail.com>
2024-06-03 15:33:12 +0200

    h1 dataset temp

--- datasets/ETTh1.py
+++ datasets/ETTh1.py
@@ -1,4 +1,5 @@
 from benchopt import BaseDataset, safe_import_context
+from benchmark_utils import *
 
 with safe_import_context() as import_ctx:
     import numpy as np
@@ -8,6 +9,13 @@ with safe_import_context() as import_ctx:
 
 class Dataset(BaseDataset):
     name = "ETTh1"
+
+    train_ratio = 0.7
+    val_ratio = 0.20
+    test_ratio = 0.10
+    window_size = 100
+    step = 10
+    horizon = 96
 
     requirements = ["numpy"]
 
@@ -21,8 +29,29 @@ class Dataset(BaseDataset):
             """
         )
         data = pd.read_csv(os.join(data_path, "ETTh1.csv"))
-        X = data.drop("OT", axis=1).values
-        X = np.array(X).T
 
-        y = data["OT"].values
-        return dict(X=X, y=y)
+        # Split the data
+        n = len(data)
+        n_train = int(n * self.train_ratio)
+        n_val = int(n * self.val_ratio)
+        n_test = n - n_train - n_val
+
+        X_train = data[:n_train]
+        X_val = data[n_train : n_train + n_val]
+        X_test = data[n_train + n_val :]
+
+        # Need to scale data first
+        X_train, X_val, X_test = scale_data(X_train, X_val, X_test)
+
+        # Data shape is (n_windows, n_features, window_size)
+        X_train, y_train = data_windowing(
+            X_train, self.window_size, self.step, self.horizon
+        )
+
+        X_val, y_val = data_windowing(X_val, self.window_size, self.step, self.horizon)
+
+        X_test, y_test = data_windowing(
+            X_test, self.window_size, self.step, self.horizon
+        )
+
+        return dict(X=(X_train, X_val, X_test), y=(y_train, y_val, y_test))
