3263caeb12757dc2b10494c1decb2c3197c79c63
jadyehya <jadyehya@hotmail.com>
2024-06-05 14:01:38 +0200

    changed installing channel for skforecast

--- solvers/skforecast_multiseries_lasso.py
+++ solvers/skforecast_multiseries_lasso.py
@@ -33,7 +33,7 @@ class Solver(BaseSolver):
     # List of packages needed to run the solver. See the corresponding
     # section in objective.py
     install_cmd = "conda"
-    requirements = ["scikit-learn", "skforecast"]
+    requirements = ["scikit-learn", "pip:skforecast"]
 
     def set_objective(self, X, y):
         # Define the information received by each solver from the objective.
