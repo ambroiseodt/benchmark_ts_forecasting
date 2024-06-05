045169b24c9803ccd1cb1e6c808899fd355aeb99
Cédric Allain <cedric.allain31@gmail.com>
2024-06-05 16:18:43 +0200

    Implem cv (#36)
    * add benchopt cross-validation
    
    * update solver_template and metric evaluation
    
    * update evaluate_results
    
    * update
    
    * update
    
    * update
    
    * remove useless comments
    
    ---------
    
    Co-authored-by: Cédric Allain <CedricAllainEnsae@users.noreply.github.com>
    Co-authored-by: Jad <64837518+Jad-yehya@users.noreply.github.com>

--- objective.py
+++ objective.py
@@ -6,6 +6,7 @@ from benchopt import BaseObjective, safe_import_context
 with safe_import_context() as import_ctx:
     import numpy as np
     from benchmark_utils import mse, mae
+    from sklearn.model_selection import TimeSeriesSplit
 
 
 # The benchmark objective must be named `Objective` and
@@ -18,6 +19,21 @@ class Objective(BaseObjective):
     # URL of the main repo for this benchmark.
     url = "https://github.com/ambroiseodt/benchmark_ts_forecasting"
 
+    # List of parameters for the objective. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    # This means the OLS objective will have a parameter `self.whiten_y`.
+    parameters = {
+        # Parameters for cv-split
+        "n_splits": [5],  # number of folds in the cross-validation split
+        # Parameters for evaluate results
+        "eval_window_size": [512],  # size of the initial window for prediction
+        "fixed_window_size": [
+            True
+        ],  # If False, will increase the window by 'horizon' steps after each sub-evaluation
+        "horizon": [32],  # number of step to predict
+    }
+
     # List of packages needed to run the benchmark.
     # They are installed with conda; to use pip, use 'pip:packagename'. To
     # install from a specific conda channel, use 'channelname:packagename'.
@@ -28,6 +44,7 @@ class Objective(BaseObjective):
     install_cmd = "conda"
     requirements = ["scikit-learn"]
 
+
     # Minimal version of benchopt required to run this benchmark.
     # Bump it up if the benchmark depends on a new feature of benchopt.
     min_benchopt_version = "1.6"
@@ -37,24 +54,87 @@ class Objective(BaseObjective):
         # returned by `Dataset.get_data`. This defines the benchmark's
         # API to pass data. This is customizable for each benchmark.
         self.X, self.y = X, y
+        self.n_samples, self.n_features = X.shape
 
-    def evaluate_result(self, pred):
+        # Specify a cross-validation splitter as the `cv` attribute.
+        # This will be automatically used in `self.get_split` to split
+        # the arrays provided.
+        self.cv = TimeSeriesSplit(
+            n_splits=self.n_splits,
+            max_train_size=None,
+            test_size=None,
+            gap=0,
+        )
+
+        # Compute length of one fold
+        self.n_samples_fold = int(self.n_samples / (self.n_splits + 1))
+
+        assert (
+            self.eval_window_size + self.horizon <= self.n_samples_fold
+        ), "The number of data in each fold is smaller than the needed number of data for evaluation. Decrease the number of splits and/or the size of the evaluation window size."
+
+        # If the cross-validation requires some metadata, it can be
+        # provided in the `cv_metadata` attribute. This will be passed
+        # to `self.cv.split` and `self.cv.get_n_splits`.
+
+    def evaluate_result(self, model):
+        """
+        model : instance of ForecastModel that has a .predict() method
+        """
         # The keyword arguments of this function are the keys of the
         # dictionary returned by `Solver.get_result`. This defines the
         # benchmark's API to pass solvers' result. This is customizable for
         # each benchmark.
 
-        Y_train = self.y[0]  # keep only train data
+        def get_pred(X):
+            """
+            Compute the sliding forecast predictions for a given nd.array
+            """
+            n_pred = int(
+                (X.shape[0] - self.eval_window_size) / self.horizon
+            )  # number of sub-evaluation possible
 
-        loss_mse = mse(pred, Y_train)  # ((Y_train - pred) ** 2).mean()
+            # Initialize the data to compute the prediction from
+            new_data = X[: self.eval_window_size]
 
-        loss_mae = mae(pred, Y_train)  # np.abs(Y_train - pred).mean()
+            predictions = []
+            for i in range(n_pred):
+                predictions.append(model.predict(new_data, horizon=self.horizon))
+
+                next_data = X[
+                    self.eval_window_size
+                    + i * self.horizon : self.eval_window_size
+                    + (i + 1) * self.horizon
+                ]
+                new_data = np.r_[new_data, next_data]
+
+                if self.fixed_window_size:
+                    # Remove the first samples to keep new_data at a fixed size
+                    new_data = new_data[self.horizon :]
+
+            return np.concatenate(predictions)
+
+        def compute_metrics_from_pred(X, pred):
+            true_data = X[self.eval_window_size :]
+            return mse(pred, true_data), mae(pred, true_data)
+
+        # Compute the evaluation metrics on the train data
+        pred_train = get_pred(self.X_train)
+        loss_mse_train, loss_mae_train = compute_metrics_from_pred(
+            self.X_train, pred_train
+        )
+
+        # Compute the evaluation metrics on the test data
+        pred_test = get_pred(self.X_test)
+        loss_mse_test, loss_mae_test = compute_metrics_from_pred(self.X_test, pred_test)
 
         # This method can return many metrics in a dictionary. One of these
         # metrics needs to be `value` for convergence detection purposes.
         return dict(
-            value=loss_mse,
-            loss_mae=loss_mae,
+            value=loss_mse_train,
+            loss_mae_train=loss_mae_train,
+            loss_mse_test=loss_mse_test,
+            loss_mae_test=loss_mae_test,
         )
 
     def get_one_result(self):
@@ -68,7 +148,26 @@ class Objective(BaseObjective):
         # for `Solver.set_objective`. This defines the
         # benchmark's API for passing the objective to the solver.
         # It is customizable for each benchmark.
+
+        # Call `self.get_split` with the arrays to split.
+        # This method default behavior behave like sklearn's
+        # `train_test_split`, splitting the input arrays using
+        # the indexes returned by `self.cv.split`.
+        self.X_train, self.X_test = self.get_split(self.X)
+
+        # How it works:
+        # 1) get_split() returns 1 fold
+        # 2) the solver is run on the X_train of the current fold
+        # 3) the solver returns the model, and we can cumpute the predictions
+        # on X_train and X_test in the evaluate_result() method.
+        # 4) the solver is run until there is convergence
+        # 5) Once convergence is reached, get_split() is called again, and then
+        # returns the next fold.
+
+        # Only the full train data, of shape (n_sample_fold, n_features),
+        # is passed to the solver.
         return dict(
-            X=self.X,
-            y=self.y,
+            X_train=self.X_train,
+            # Possibly, here we can send X_test to the solver, as follows:
+            # X_test=self.X_test
         )
---
+++ solvers/solver_template.py
@@ -0,0 +1,80 @@
+from benchopt import BaseSolver, safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    import numpy as np
+
+    # import your reusable functions here
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(BaseSolver):
+
+    # Name to select the solver in the CLI and to display the results.
+    name = "solver_template"
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    parameters = {
+        "param1": [1],
+    }
+
+    # List of packages needed to run the solver. See the corresponding
+    # section in objective.py
+    requirements = []
+
+    def set_objective(self, X_train):
+        # Define the information received by each solver from the objective.
+        # The arguments of this function are the results of the
+        # `Objective.get_objective`. This defines the benchmark's API for
+        # passing the objective to the solver.
+        # It is customizable for each benchmark.
+
+        self.X_train = X_train  # shape (n_samples_train, n_features)
+
+        # Here, do desire pre-processing of X_train
+
+    def run(self, n_iter):
+        # This is the function that is called to evaluate the solver.
+        # It runs the algorithm for a given a number of iterations `n_iter`.
+        # You can also use a `tolerance` or a `callback`, as described in
+        # https://benchopt.github.io/performance_curves.html
+
+        # Fit develop forecast method
+        method.fit(self.X_train, n_iter)
+
+    class ForecastModel:
+        def __init__(self, method):
+            self.method = method
+
+        def predict(new_data, horizon):
+            """
+            Function that, given data, predicts the next steps
+
+            Parameters
+            ----------
+            new_data : np.ndarray, shape (n_samples, n_features)
+                The data to predict from
+
+            horizon : int
+                The number of steps to predict
+
+            Returns
+            -------
+            pred : np.ndarray, shape (horizon, n_features)
+                The predicted data
+            """
+            raise NotImplementedError
+
+    def get_result(self):
+        model = ForecastModel(method)
+        # Return the result from one optimization run.
+        # The outputs of this function is a dictionary which defines the
+        # keyword arguments for `Objective.evaluate_result`
+        # This defines the benchmark's API for solvers' results.
+        # it is customizable for each benchmark.
+        return dict(model=model)
